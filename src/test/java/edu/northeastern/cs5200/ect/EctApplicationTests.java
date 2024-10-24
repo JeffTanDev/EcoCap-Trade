6d6b023602ac672d44e518a8eaf3e951c5570515
JeffTanChina <jeff.tandev@gmail.com>
2024-10-24 16:59:26 -0700

    Initial commit

---
+++ src/test/java/edu/northeastern/cs5200/ect/EctApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs5200.ect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
